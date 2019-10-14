06bd796a27aeaf93fb58dd631b9d8cecc5255300
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-10-14 17:15:45 -0300

    11. Javascript Module Systems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create a react component
+
+
+// Take the react component and show it on the screen
