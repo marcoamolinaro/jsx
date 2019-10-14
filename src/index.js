764234026772d79b25c362af9906f39116d8756c
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-10-14 18:02:53 -0300

    16. Converting HTML to JSX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = function () {
-    return <div>Hello World!</div>;
+    return (        
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style="background-color: blue; color: white;">Submit</button>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen
