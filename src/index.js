0ececf1267a65d39ee7b5bac630d3c3976ca4caa
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-10-14 18:24:57 -0300

    18. Converting Styling to JSX Format

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const App = function () {
         <div>
             <label class="label" for="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style="background-color: blue; color: white;">Submit</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
         </div>
     );
 };
